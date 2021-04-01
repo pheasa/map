59f20a34d8856392b4219ae199454c0f0feeba73
PheakPhasa <44224248+pheasa@users.noreply.github.com>
2021-04-01 11:40:44 +0700

    update

---
+++ jquery/demo_test_post.php
@@ -0,0 +1,7 @@
+<?php 
+
+print_r($_GET);
+print_r($_POST);
+// echo json_encode($_POST);
+
+?>
---
+++ jquery/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<script async src="https://telegram.org/js/telegram-widget.js?14" data-telegram-login="TahReanBot" data-size="large" data-auth-url="http://localhost/test/jquery/demo_test_post.php" data-request-access="write"></script>
+</body>
+</html>
